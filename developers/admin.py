de82145e0d0454842ea5cfc00957e1352b4f7ac7
adampgiles <adampgiles@outlook.com>
2023-03-26 11:47:56 +0100

    models: added Post model and customised admin

--- developers/admin.py
+++ developers/admin.py
@@ -3,5 +3,22 @@ from .models import Developer
 from .models import Category
 
 # Register your models here.
-admin.site.register(Developer)
-admin.site.register(Category)
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = (
+        'profile_name',
+        'category',
+        'price',
+        'count_sold',
+        'image',
+    )
+
+    ordering = ('profile_name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+admin.site.register(Developer, DeveloperAdmin)
+admin.site.register(Category, CategoryAdmin)
--- developers/models.py
+++ developers/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,12 +17,26 @@ class Category(models.Model):
     
 class Developer(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
-    description = models.TextField()
+    first_name = models.CharField(max_length=30, default='first_name')
+    last_name = models.CharField(max_length=30, default='last_name')
+    email = models.EmailField(null=True, blank=True)
+    profile_name = models.CharField(max_length=254)
+    description = models.TextField(max_length=508)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    count_sold = models.IntegerField()
+    count_sold = models.IntegerField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.profile_name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    publish_date = models.DateTimeField(auto_now_add=True, blank=True)
+    content = models.CharField(max_length=1000)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
