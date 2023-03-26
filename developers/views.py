63b4a25fa59f1c61f76d3078a90223bc40cb39b1
adampgiles <adampgiles@outlook.com>
2023-03-26 11:58:53 +0100

    feat: Setup Developer view and template

--- developers/views.py
+++ developers/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Developer
 
 # Create your views here.
+def all_developers(request):
+    """ View to show all developers """
+
+    developers = Developer.objects.all()
+
+    context = {
+        'developers': developers,
+    }
+
+    return render(request, 'developers/developers.html', context)
