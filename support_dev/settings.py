85d11237694ebfeb70c453827c4c30f35fd4b7fb
adampgiles <adampgiles@outlook.com>
2023-04-02 18:07:33 +0100

    feat: added checkout views and template

--- support_dev/settings.py
+++ support_dev/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'home',
     'developers',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
--- support_dev/urls.py
+++ support_dev/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('developers/', include('developers.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
