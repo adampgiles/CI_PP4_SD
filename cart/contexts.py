dd51d3017fc67e09b421f9ac06a28b98cfde9772
adampgiles <adampgiles@outlook.com>
2023-04-02 11:44:38 +0100

    feat: add to cart functionality added

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,6 +1,21 @@
+from django.shortcuts import get_object_or_404
+from developers.models import Developer
+
 def cart_contents(request):
+    
     cart_items = []
     total = 0
+    product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        developer = get_object_or_404(Developer, pk=item_id)
+        total += developer.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'developer': developer,
+        })
     
     context = {
         'cart_items': cart_items,
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,15 @@ def view_cart(request):
     """ View to show cart """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    cart[item_id] = int(1)
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
