21a2ab93fdf8a73e9b3b2e2c8adcbc67b47e6b4a
adampgiles <adampgiles@outlook.com>
2023-04-02 17:21:32 +0100

    feat: added messages functionality to display text alerts

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
+from developers.models import Developer
 # Create your views here.
 
 def view_cart(request):
@@ -10,23 +12,27 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add item to the shopping cart """
 
+    developer = get_object_or_404(Developer, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     cart[item_id] = int(1)
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+
+    messages.success(request, f'{developer.profile_name} - £{developer.price} added to cart')
     return redirect(redirect_url)
 
 def remove_from_cart(request, item_id):
     """ Remove item from the shopping cart """
 
+    developer = get_object_or_404(Developer, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     cart.pop(item_id)
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+
+    messages.success(request, f'{developer.profile_name} - £{developer.price} removed from cart')
     return redirect(redirect_url)
