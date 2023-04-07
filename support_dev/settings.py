633f6943fc63ddc044d94a86ad9570e20b2991c9
adampgiles <adampgiles@outlook.com>
2023-04-07 14:55:42 +0100

    fix: collectstatic not working, installed boto3 and updated AWS bucket name and policy

--- support_dev/settings.py
+++ support_dev/settings.py
@@ -165,22 +165,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Configuration
-    AWS_BUCKET_NAME = 'support-dev-project'
-    AWS_REGION_NAME = 's3.eu-west-2'
+    AWS_STORAGE_BUCKET_NAME = 'supportdev-bucket'
+    AWS_REGION_NAME = 's3.eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'http://{AWS_BUCKET_NAME}.s3-website.{AWS_REGION_NAME}.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_REGION_NAME}.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage' 
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage' 
     MEDIAFILES_LOCATION = 'media'
-    AWS_DEFAULT_ACL = None
-    
+
     # Override static and media URLS in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
  # Stripe Configuration
