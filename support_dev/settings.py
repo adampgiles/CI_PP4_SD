693209b80b58ab4ba831482e63feb15b8f309fd3
adampgiles <adampgiles@outlook.com>
2023-04-07 14:14:56 +0100

    code: updated AWS config

--- support_dev/settings.py
+++ support_dev/settings.py
@@ -169,14 +169,15 @@ if 'USE_AWS' in os.environ:
     AWS_REGION_NAME = 's3.eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'http://{AWS_BUCKET_NAME}.s3-website.{AWS_REGION_NAME}.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage' 
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage' 
     MEDIAFILES_LOCATION = 'media'
-
+    AWS_DEFAULT_ACL = None
+    
     # Override static and media URLS in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
